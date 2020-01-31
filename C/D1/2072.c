f7334a17055c38a2299f6f8d72e7456e9f8ec42e
423april <423april@korea.ac.kr>
2020-01-31 11:30:36 +0900

    Create 2072.c

---
+++ C/D1/2072.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int T;
+	int num;
+	int sum = 0;
+	
+	scanf("%d", &T);
+	for(int i = 1; i < T; i++){
+		printf("#%d ", i);
+		for(int j = 0; j < 10; j++){
+			scanf("%d ", &num);
+			if(num%2 != 0) sum += num;
+		}
+		printf("%d\n", sum);
+		sum = 0;
+	}
+}
